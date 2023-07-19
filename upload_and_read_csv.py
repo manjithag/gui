cfbf4f57f5effa0e1f32681dfecd305f9a612f3f
Manjitha <manjitha.ee@gmail.com>
2023-07-19 22:31:29 +0200

    Add list, label and button

--- upload_and_read_csv.py
+++ upload_and_read_csv.py
@@ -1,17 +1,42 @@
 import tkinter as tk
+import pandas as pd
+from tkinter import filedialog
+# read csv files through tkinter dialog window
+from tkinter import *
+from tkinter.ttk import *
 
-class gui:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.textbox = tk.Text(self.root, height=2, font=('Arial', 16))
-        self.textbox.pack(padx=10, pady=10)
+from tkinter.filedialog import askopenfile
+import pandas as pd
 
-        self.button = tk.Button(self.root, text='Click here', font=('Arial',12), command=self.show_message)
-        self.button.pack()
+root = Tk()
+root.geometry('600x600')
 
-        self.root.mainloop()
+var = 'Attri'
 
-    def show_message(self):
-        print(self.textbox.get('1.0', tk.END))
+attribute = Label(root, text = var )
+attribute.pack(pady=10, padx=10)
 
-gui()
+listbox = Listbox(root)
+
+
+listbox.pack()
+
+def open_file():
+    file = askopenfile(mode='r', filetypes = [('csv files', '*.csv')])
+    if file is not None:
+        content = pd.read_csv(file)
+        print(content.head()) # this will print in the terminal
+        var = list(content.head(1))
+
+        for i in var:
+            listbox.insert(1, i)
+        print(var)
+
+btn = Button(root, text='open', command = open_file)
+btn.pack(side = TOP, pady = 10)
+
+
+
+
+
+mainloop()
